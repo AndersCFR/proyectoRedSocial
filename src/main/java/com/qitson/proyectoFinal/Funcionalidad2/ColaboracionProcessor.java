751a1507cf1adffff758dab7cd5b9fd352a14434
alejandrollanganate <luis.llanganate@epn.edu.ec>
2021-01-17 14:24:24 -0500

    REFACTORING (Extract Method): Clase ColaboracionProcessor, método realizar solicitudColaboracion

--- src/main/java/com/qitson/proyectoFinal/Funcionalidad2/ColaboracionProcessor.java
+++ src/main/java/com/qitson/proyectoFinal/Funcionalidad2/ColaboracionProcessor.java
@@ -13,17 +13,22 @@ public class ColaboracionProcessor {
     }
 
     public boolean realizarSolicitudColaboracion(Proyecto proyecto, Colaborador colaborador, Aporte aporte){
-        ColaboracionResponse res = gateway.requestColaboracion(
-                new ColaboracionRequest(proyecto, colaborador, aporte)
-        );
+        ColaboracionResponse res = gateway.requestColaboracion(new ColaboracionRequest(proyecto, colaborador, aporte));
+        BuscadorProyectos buscadorProyectos = new BuscadorProyectos();
+        return verificarCondicionesResponse(proyecto, colaborador, aporte, res, buscadorProyectos);
+    }
+
+    private boolean verificarCondicionesResponse(Proyecto proyecto, Colaborador colaborador, Aporte aporte, ColaboracionResponse res, BuscadorProyectos buscadorProyectos) {
         BuscadorProyectos b = new BuscadorProyectos();
-        if(res.getStatus() == ColaboracionResponse.ColaboracionEstado.OK
-                && b.verificarExistenciaProyecto(Data.getDataProyectos(), proyecto)
-                && proyecto.get_estado() == Proyecto.Estado.BUSCANDO_COLABORACION){
-            Data.solicitudes.add(new ColaboracionRequest(proyecto, colaborador, aporte));
+        if(res.getStatus() == ColaboracionResponse.ColaboracionEstado.OK && b.verificarExistenciaProyecto(Data.getDataProyectos(), proyecto)  && proyecto.get_estado() == Proyecto.Estado.BUSCANDO_COLABORACION){
+            crearSolicitud(proyecto, colaborador, aporte);
             return true;
         }
         return false;
+    }
+
+    private void crearSolicitud(Proyecto proyecto, Colaborador colaborador, Aporte aporte){
+        Data.solicitudes.add(new ColaboracionRequest(proyecto, colaborador, aporte));
     }
 }
 
